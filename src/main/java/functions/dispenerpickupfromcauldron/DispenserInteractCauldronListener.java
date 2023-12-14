9ef016b9fe735083b4e426e1b7457470170e6381
karl-k-m <karl.k.muldia@gmail.com>
2023-12-14 06:09:03 +0200

    fuckery

--- src/main/java/functions/dispenerpickupfromcauldron/DispenserInteractCauldronListener.java
+++ src/main/java/functions/dispenerpickupfromcauldron/DispenserInteractCauldronListener.java
@@ -4,17 +4,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.block.BlockState;
 import org.bukkit.block.Dispenser;
 import org.bukkit.block.data.Directional;
-import org.bukkit.block.data.Levelled;
-import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.material.Cauldron;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
@@ -29,18 +25,7 @@ public class DispenserInteractCauldronListener implements Listener {
     @EventHandler
     public void onDispenserInteractCauldron(BlockDispenseEvent event) {
         if (event.getItem().getType() == Material.BUCKET) {
-            Directional directional = (Directional) event.getBlock().getBlockData();
-            BlockFace facing = directional.getFacing();
-
-            int x = event.getBlock().getX();
-            int y = event.getBlock().getY();
-            int z = event.getBlock().getZ();
-
-            int targetX = x + facing.getModX();
-            int targetY = y + facing.getModY();
-            int targetZ = z + facing.getModZ();
-
-            Block block = Bukkit.getWorld(event.getBlock().getWorld().getUID()).getBlockAt(targetX, targetY, targetZ);
+            Block block = getTargetBlock(event);
 
             if (block.getType() == Material.LAVA_CAULDRON) {
                 event.setCancelled(true);
@@ -57,18 +42,7 @@ public class DispenserInteractCauldronListener implements Listener {
         }
 
         if (event.getItem().getType() == Material.BUCKET) {
-            Directional directional = (Directional) event.getBlock().getBlockData();
-            BlockFace facing = directional.getFacing();
-
-            int x = event.getBlock().getX();
-            int y = event.getBlock().getY();
-            int z = event.getBlock().getZ();
-
-            int targetX = x + facing.getModX();
-            int targetY = y + facing.getModY();
-            int targetZ = z + facing.getModZ();
-
-            Block block = Bukkit.getWorld(event.getBlock().getWorld().getUID()).getBlockAt(targetX, targetY, targetZ);
+            Block block = getTargetBlock(event);
 
             // TODO: Make this work for only full water cauldrons.
             if (block.getType() == Material.WATER_CAULDRON) {
@@ -86,18 +60,7 @@ public class DispenserInteractCauldronListener implements Listener {
         }
 
         if (event.getItem().getType() == Material.WATER_BUCKET) {
-            Directional directional = (Directional) event.getBlock().getBlockData();
-            BlockFace facing = directional.getFacing();
-
-            int x = event.getBlock().getX();
-            int y = event.getBlock().getY();
-            int z = event.getBlock().getZ();
-
-            int targetX = x + facing.getModX();
-            int targetY = y + facing.getModY();
-            int targetZ = z + facing.getModZ();
-
-            Block block = Bukkit.getWorld(event.getBlock().getWorld().getUID()).getBlockAt(targetX, targetY, targetZ);
+            Block block = getTargetBlock(event);
 
             if (block.getType() == Material.CAULDRON) {
                 event.setCancelled(true);
@@ -116,18 +79,7 @@ public class DispenserInteractCauldronListener implements Listener {
         }
 
         if (event.getItem().getType() == Material.LAVA_BUCKET) {
-            Directional directional = (Directional) event.getBlock().getBlockData();
-            BlockFace facing = directional.getFacing();
-
-            int x = event.getBlock().getX();
-            int y = event.getBlock().getY();
-            int z = event.getBlock().getZ();
-
-            int targetX = x + facing.getModX();
-            int targetY = y + facing.getModY();
-            int targetZ = z + facing.getModZ();
-
-            Block block = Bukkit.getWorld(event.getBlock().getWorld().getUID()).getBlockAt(targetX, targetY, targetZ);
+            Block block = getTargetBlock(event);
 
             if (block.getType() == Material.CAULDRON) {
                 event.setCancelled(true);
@@ -146,15 +98,29 @@ public class DispenserInteractCauldronListener implements Listener {
         }
     }
 
+    private Block getTargetBlock(BlockDispenseEvent event) {
+        Directional directional = (Directional) event.getBlock().getBlockData();
+        BlockFace facing = directional.getFacing();
+
+        int x = event.getBlock().getX();
+        int y = event.getBlock().getY();
+        int z = event.getBlock().getZ();
+
+        int targetX = x + facing.getModX();
+        int targetY = y + facing.getModY();
+        int targetZ = z + facing.getModZ();
+
+        Block block = Bukkit.getWorld(event.getBlock().getWorld().getUID()).getBlockAt(targetX, targetY, targetZ);
+
+        return block;
+    }
+
     private static void removeItemFromDispenser(Inventory inv, ItemStack item) {
         for (int i = 0; i < inv.getSize(); i++) {
             ItemStack itemStack = inv.getItem(i);
-            if (itemStack != null && itemStack.isSimilar(item)) {
-                if (itemStack.getAmount() == 1) {
-                    inv.setItem(i, null);
-                } else {
-                    itemStack.setAmount(itemStack.getAmount() - 1);
-                }
+            if (itemStack != null && itemStack.getType() == item.getType()) {
+                itemStack.setAmount(itemStack.getAmount() - 1);
+                inv.setItem(i, itemStack);
                 break;
             }
         }
@@ -168,12 +134,9 @@ public class DispenserInteractCauldronListener implements Listener {
                 break;
             }
 
-            // If inventory is full, drop item on ground.
             else if (i == inv.getSize() - 1) {
                 Objects.requireNonNull(Objects.requireNonNull(inv.getLocation()).getWorld()).dropItem(inv.getLocation(), item);
             }
         }
     }
 }
-
-//Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "say lol");
